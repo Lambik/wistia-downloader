b8a5d477f8a97229ed37ce5710c6cfae238243a8
Tom <lambik@gmail.com>
2021-11-29 22:21:53 +0100

    Code layout

--- audioguy-downloader.user.js
+++ audioguy-downloader.user.js
@@ -11,63 +11,67 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+	'use strict';
 
-    function parseVideos() {
-        let viddivs = document.getElementsByClassName('wistia_embed');
+	function parseVideos() {
+		let viddivs = document.getElementsByClassName('wistia_embed');
 
-        for (let viddiv of viddivs) {
-            let wistiaId = viddiv.id.split('_')[1];
+		for (let viddiv of viddivs) {
+			let wistiaId = viddiv.id.split('_')[1];
 
-            GM_xmlhttpRequest({
-                                  method: 'GET',
-                                  url: 'https://fast.wistia.net/embed/iframe/' + wistiaId,
-                                  onerror: function(result) {
-                                      console.error('xmlrequest error', result);
-                                  },
-                                  onabort: function(result) {
-                                      console.error('xmlrequest abort', result);
-                                  },
-                                  onload: function(result) {
-                                      let body = result.responseText;
-                                      let assets = body.match(/"assets":(\[.*?\])/);
-                                      if (assets.length) {
-                                          let allvids = JSON.parse(assets[1]);
-                                          // console.log(allvids);
-                                          let newNode = document.createElement('div');
-                                          let str = "<h4>Download options:</h4><ul>";
-                                          for (let vid of allvids) {
-                                              if (vid.type != 'hls_video' && vid.public) {
-                                                  str += '<li style="font-size: 1rem;">' + vid.display_name + ' (' + vid.width + 'x' + vid.height + ', ' + vid.ext + '): <a href="' + vid.url + '" target="_blank">' + formatBytes(vid.size) + '</a></li>';
-                                              }
-                                          }
-                                          str += "</ul>";
-                                          newNode.innerHTML = str;
-                                          newNode.style.color = 'white';
-                                          newNode.style.fontSize = 'auto';
-                                          viddiv.parentNode.parentNode.parentNode.appendChild(newNode)
-                                      }
-                                      else {
-                                          console.error('wista returned invalid data', body);
-                                      }
-                                  }
-                              });
-        }
-    }
+			GM_xmlhttpRequest(
+				{
+					method : 'GET',
+					url    : 'https://fast.wistia.net/embed/iframe/' + wistiaId,
+					onerror: function (result) {
+						console.error('xmlrequest error', result);
+					},
+					onabort: function (result) {
+						console.error('xmlrequest abort', result);
+					},
+					onload : function (result) {
+						let body = result.responseText;
+						let assets = body.match(/"assets":(\[.*?\])/);
+						if (assets.length) {
+							let allvids = JSON.parse(assets[1]);
+							// console.log(allvids);
+							let newNode = document.createElement('div');
+							let str = "<h4>Download options:</h4><ul>";
+							for (let vid of allvids) {
+								if (vid.type != 'hls_video' && vid.public) {
+									str += '<li style="font-size: 1rem;">' + vid.display_name + ' (' + vid.width + 'x' + vid.height + ', ' + vid.ext + '): <a href="' + vid.url + '" target="_blank">' + formatBytes(vid.size) + '</a></li>';
+								}
+							}
+							str += "</ul>";
+							newNode.innerHTML = str;
+							newNode.style.color = 'white';
+							newNode.style.fontSize = 'auto';
+							viddiv.parentNode.parentNode.parentNode.appendChild(newNode)
+						}
+						else {
+							console.error('wista returned invalid data', body);
+						}
+					}
+				}
+			);
+		}
+	}
 
-    // thanks to https://stackoverflow.com/a/18650828/102720
-    function formatBytes(bytes, decimals = 2) {
-        if (bytes === 0) return '0 Bytes';
+	// thanks to https://stackoverflow.com/a/18650828/102720
+	function formatBytes(bytes, decimals = 2) {
+		if (bytes === 0) {
+			return '0 Bytes';
+		}
 
-        const k = 1024;
-        const dm = decimals < 0 ? 0 : decimals;
-        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		const k = 1024;
+		const dm = decimals < 0 ? 0 : decimals;
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-        const i = Math.floor(Math.log(bytes) / Math.log(k));
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-    }
+		return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+	}
 
-    parseVideos();
+	parseVideos();
 })();
